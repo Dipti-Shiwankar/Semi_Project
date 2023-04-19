7bb37506faf2a30980f51bcf0541cd0422edbff1
nehapatil3071 <nehapatil3071@gmail.com>
2023-04-19 19:26:48 +0530

    Neha

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,12 +3,9 @@ import Head from 'next/head'
 import Image from 'next/image'
 import styles from './page.module.css'
 import tw from 'tailwind-styled-components'
-import Car from 'C:/Users/Dipti/Downloads/Car2.png';
 import mapboxgl from 'mapbox-gl';
 import { useEffect,useState,useRef } from 'react'
-import Map from './components/Map'
-import Link from 'next/link';
-import Search from './components/Search';
+import clogo from 'D:/Project/git/Semi_Project/Images/logo.png'
 
 mapboxgl.accessToken='pk.eyJ1IjoiZGlwdGlzaGl3YW5rYXIyOTEwIiwiYSI6ImNsZ2tvNHd2MTAzNHczZ29teTQ2dGRnNzUifQ.e_mPzKv5Yv8FsHz-Rt0_Vg';
 
@@ -17,46 +14,60 @@ mapboxgl.accessToken='pk.eyJ1IjoiZGlwdGlzaGl3YW5rYXIyOTEwIiwiYSI6ImNsZ2tvNHd2MTA
 
 export default function Home() {
 
- 
+  const mapContainer = useRef(null);
+const map = useRef(null);
+const [lng, setLng] = useState(-70.9);
+const [lat, setLat] = useState(42.35);
+const [zoom, setZoom] = useState(9);
+
+
+  useEffect(() => {
+    
+    const map = new mapboxgl.Map({
+      container:"map",
+      style: 'mapbox://styles/drakosi/ckvcwq3rwdw4314o3i2ho8tph',
+      center: [72.8691, 19.0684],
+      zoom: 7,
+    });
+
+    
+  });
 
   return (
     <Wrapper>
-      <Map />
+      <Map id='map'>Map</Map>
       <ActionItem>
         <Header>
-          <Logo src="https://img.freepik.com/premium-vector/letter-c-car-modern-logo-symbol-icon-vector-graphic-design_15473-496.jpg?w=360"/>
+          <Logo src="https://www.linkpicture.com/q/taxi-round-white-icon-logo-png-11663597162dsee73c6lp-removebg-preview.png"/>
           <Profile>
             <Name>Uber Ridder</Name>
-            <UserImage src="https://www.nicepng.com/png/detail/128-1280406_view-user-icon-png-user-circle-icon-png.png"/>
-           </Profile> 
+            <UserImage src="https://p7.hiclipart.com/preview/782/114/405/5bbc3519d674c.jpg"/>
+          </Profile> 
         </Header>
-        
         <ActionButtons>
-        <Link href="/Search">
-          
-            <ActionButton> <ActionButtonImage src='{Car}'/>
-              Ride
-            </ActionButton> 
-        </Link>
-            <ActionButton> <ActionButtonImage src="https://static.vecteezy.com/system/resources/previews/006/059/929/original/symbol-of-a-bike-isolated-on-white-background-bike-outline-icon-free-vector.jpg"/>
-              Wheel
-            </ActionButton>
-            <ActionButton><ActionButtonImage src="https://img.freepik.com/free-icon/calendar-icon-black_318-9776.jpg"/>
-              Reservation
-            </ActionButton>
+          <ActionButton> <ActionButtonImage src="https://www.linkpicture.com/q/ride.png"/>
+            Ride
+          </ActionButton> 
+          <ActionButton> <ActionButtonImage src="https://www.linkpicture.com/q/bike_1.png"/>
+            Wheel
+          </ActionButton>
+          <ActionButton><ActionButtonImage src="https://www.linkpicture.com/q/date_6.png"/>
+            Reservation
+          </ActionButton>
 
         </ActionButtons>
-        
-        <InputButton>Where to ?
-        </InputButton>
-        <Link href='/Search'>search</Link>
        </ActionItem>
     </Wrapper>
   )
 }
 
+
+
 const Wrapper = tw.div`
-flex flex-col  h-screen
+flex flex-col h-screen
+`
+const Map = tw.div`
+flex-1
 `
 const ActionItem = tw.div`
 flex-1 p-4
@@ -65,7 +76,7 @@ const Header = tw.div`
 flex justify-between items-center
 `
 const Logo = tw.img`
-h-28
+h-20 w-20 p-px padding:2px
 `
 
 const Profile = tw.div`
@@ -73,22 +84,18 @@ flex items-center
 `
 
 const Name = tw.div`
-mr-4 w-20 text-sm text-gray-900
+mr-4 w-20 text-sm
 `
 
 const UserImage = tw.img`
 h-12 w-12 rounded-full border border-gray-200 p-px
 `
 const ActionButtons = tw.div`
-flex
+flex 
 `
 const ActionButton = tw.div`
-flex  bg-gray-200 flex-1 m-1 h-32 items-center flex-col justify-center rounded-lg transform hover:scale-105 transition text-xl text-gray-900
+flex bg-gray-200 flex-1 m-1 h-32 items-center flex-col justify-center rounded-lg transform hover:scale-105 transition text-xl text-gray-900
 `
 const ActionButtonImage = tw.img`
-h-3/5 
-`
-
-const InputButton= tw.div`
-h-20 bg-gray-200 text-2xl text-gray-900 p-4 flex items-center mt-8
+h-3/5
 `
